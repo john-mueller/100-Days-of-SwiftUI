9c428b6cad3cce4d349ac7053d058a3f0d43a559
John Mueller <jmuellerokc@gmail.com>
2019-10-09 05:48:53 -0500

    Day 16 - Added WeSplit project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by John Mueller on 10/9/19.
+//  Copyright © 2019 John Mueller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
