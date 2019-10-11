f699142c7fef37f71b40d2264cc88b92723e86fe
John Mueller <jmuellerokc@gmail.com>
2019-10-11 16:19:07 -0500

    Day 19 - Added Day19Challenge project

---
+++ Day19Challenge/Day19Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day19Challenge
+//
+//  Created by John Mueller on 10/11/19.
+//  Copyright © 2019 John Mueller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
