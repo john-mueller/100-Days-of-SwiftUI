a45db843880fa404dc4cb694332fb4741b9ebdbf
John Mueller <jmuellerokc@gmail.com>
2019-10-17 19:15:34 -0500

    Day 25 - Added Day25Challenge project

---
+++ Day25Challenge/Day25Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day25Challenge
+//
+//  Created by John Mueller on 10/17/19.
+//  Copyright © 2019 John Mueller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
