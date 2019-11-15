4948477390d60d49e25476a86890a2c9622ef05c
John Mueller <jmuellerokc@gmail.com>
2019-11-15 11:41:26 -0600

    Day 53 - Added Bookworm project

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by John Mueller on 11/15/19.
+//  Copyright © 2019 John Mueller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
